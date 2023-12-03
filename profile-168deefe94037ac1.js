958db86c3e44a0564af5547f1b6f2d43e5e0a5c9
segfo <k.segfo@gmail.com>
2023-12-03 21:31:19 +0900

    :bug: コマンドラインヘルプのバイナリ名を修正した

--- profile-168deefe94037ac1.js
+++ profile-168deefe94037ac1.js
@@ -925,7 +925,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('profile-6b3f2ecd82e5a923_bg.wasm', import.meta.url);
+        input = new URL('profile-168deefe94037ac1_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
