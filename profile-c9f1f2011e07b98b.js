fa12da0dfc5aeedb56e29946924c6936368186d0
segfo <k.segfo@gmail.com>
2023-12-03 21:40:06 +0900

    :bug: /etc/groupsになってたので修正した

--- profile-c9f1f2011e07b98b.js
+++ profile-c9f1f2011e07b98b.js
@@ -925,7 +925,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('profile-168deefe94037ac1_bg.wasm', import.meta.url);
+        input = new URL('profile-c9f1f2011e07b98b_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
